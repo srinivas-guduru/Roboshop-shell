0f1fbbbc9f0085adaba42ecb4b7cd3d3dcfbb6f9
srinivasarao guduru <srinivas.guduru97@gmail.com>
2026-02-02 13:24:12 +0530

    change

--- roboshop.sh
+++ roboshop.sh
@@ -34,3 +34,30 @@ do
     fi
 
     echo "IP Address: $IP"
+
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Updating record",
+        "Changes": [
+            {
+            "Action": "UPSERT",
+            "ResourceRecordSet": {
+                "Name": "'$RECORD_NAME'",
+                "Type": "A",
+                "TTL": 1,
+                "ResourceRecords": [
+                {
+                    "Value": "'$IP'"
+                }
+                ]
+            }
+            }
+        ]
+    }
+    '
+
+    echo "record updated for $instance"
+
+done
