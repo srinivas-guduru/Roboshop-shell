b33d31ee4ccdd4503a89f49dc17dc82be061fe67
srinivasarao guduru <srinivas.guduru97@gmail.com>
2026-02-02 12:30:32 +0530

    change

--- roboshop.sh
+++ roboshop.sh
@@ -5,29 +5,29 @@ AMI_ID="ami-0220d79f3f480ecf5"
 
 for instance in $@
 do
-    instance_id= $( aws ec2 run-instances \
+    INSTANCE_ID=$( aws ec2 run-instances \
     --image-id $AMI_ID \
-    --instance-type "t3.micro"\
+    --instance-type "t3.micro" \
     --security-group-ids $SG_ID \
     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
     --query 'Instances[0].InstanceId' \
     --output text )
 
-if [ $instance == "fornted" ]; then
-   IP=$(
-    aws ec2 decribe-instance \
-    --instance-ids $instance_id\
-    --query 'Reservations[].instances[].PublicIpAddress'\
-    --output text
+   if  [ $instance == "fornted" ]; then
+      IP=$(
+       aws ec2 decribe-instance \
+      --instance-ids $INSTANCE_ID \
+      --query 'Reservations[].instances[].PublicIpAddress' \
+      --output text
          )
    else
       IP=$(
-    aws ec2 decribe-instance \
-    --instnance-ids $instance_id\
-    --query 'Reservations[].instances[].PrivateIpAddress'\
-    --output text
-         )
-fi
+       aws ec2 decribe-instance \
+      --instnance-ids $INSTANCE_ID \
+      --query 'Reservations[].instances[].PrivateIpAddress'\
+      --output text
+          )
+   fi
 
   echo "IP Adress: $IP"
 done
